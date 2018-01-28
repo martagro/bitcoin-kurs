f46748b14de7bfc1b1332712b26bbfcee6773fe1
Marta Gronkiewicz <martamc@o2.pl>
2018-01-28 15:45:56 +0100

    podpiecie fontow
    Strona wyświetla aktualny kurs bitcoin do PLN

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
 //nazwa_funkcji jest bez nawiasów!
 //interval(nazwa_funkcji, czas)
 
-$.getJSON('https://blockchain.info/pl/ticker', function () {
-    console.log(data);
+$(document).ready(function () {
+    //refresh();
+    setInterval(refresh, 3000);
 });
+
+function refresh() {
+    $.getJSON('https://blockchain.info/pl/ticker', function (data) {
+        $('#sell').html(data.PLN.sell);
+        $('#buy').html(data.PLN.buy);
+
+        var currentBuyPrice = parseFloat($('#buy').html());
+        var currentSellPrice = parseFloat($('#sell').html());
+
+        if (currentBuyPrice > parseFloat(data.PLN.buy)) {
+            $('#buy-arrow').removeAttr('class').addClass('fa fa-arrow-down');
+        } else if (currentBuyPrice < parseFloat(data.PLN.buy)) {
+            $('#buy-arrow').removeAttr('class').addClass('fa fa-arrow-up');
+        }
+
+
+        if (currentSellPrice > parseFloat(data.PLN.sell)) {
+            $('#sell-arrow').removeAttr('class').addClass('fa fa-arrow-down');
+        } else if (currentBuyPrice < parseFloat(data.PLN.buy)) {
+            $('#buy-arrow').removeAttr('class').addClass('fa fa-arrow-up');
+        }
+
+
+        console.log('odswiezono');
+
+    });
+}
